f8fe465d9bab5991e4e50a75f4068f92d0de5266
Elton Murillo <emurillo510@gmail.com>
2016-10-23 07:22:05 -0700

    Adds adds changes for chat-messages

--- app/components/chat-messages/component.js
+++ app/components/chat-messages/component.js
@@ -1,6 +1,10 @@
 import Ember from 'ember';
 
+let herokuURL = 'https://polly-bot-server.herokuapp.com/app';
+
+
 export default Ember.Component.extend({
+    amountWorth: 0,
     isOtherChecked: false,
 
     isGreeting: true,
@@ -21,13 +25,44 @@ export default Ember.Component.extend({
 
             if(this.get('isDetermineIntermSkill')){
                 let userInput = $('#input-chatbot').val();
-                alert('hey send this to watson');
+
+                let userEducation = $('#identify-edu').html();
+                let userField = $('#identify-field').html();
+                let userIndustry = $('#identify-industry').html();
+                let userRole = $('#identify-role').html();     
+
+                alert(userEducation);
+                alert(userField);
+                alert(userIndustry);
+                alert(userRole);
+
+                let data = {
+                    "degree": userEducation,
+                    "industry": userIndustry,
+                    "major": userField,
+                    "role": userRole
+                }
+
+                console.log('data is: ', data);
+                alert('data is: ', data);
+                console.log('herokuURL', herokuURL);
+
+                Ember.$.post(herokuURL, data, function(response){
+                    let repsonse = response;
+                    console.log("RESPONSE", response);
+                    alert(response);
+                    $('#polly-message').append('<h2>Amount Worth!</h2>');
+                    $('#polly-message').append(response.guess); 
+                });
+
+
+
+
             } else if(this.get('isDetermineBeginSkill')){
                 let userInput = $('#input-chatbot').val();
                 this.toggleProperty('isDetermineIntermSkill');
                 $('#identify-determine-begin').append(userInput);
             } else if (this.get('isAdditionalSkills')){
-                alert('asdfad');
                 let userInput = $('#input-chatbot').val();
                 this.toggleProperty('isDetermineBeginSkill');
                 $('#identify-additional-skills').append(userInput);
--- config/environment.js
+++ config/environment.js
@@ -43,5 +43,29 @@ module.exports = function(environment) {
 
   }
 
+  ENV.contentSecurityPolicy = {
+  // Deny everything by default
+  'default-src': "'none'",
+
+  // Allow scripts at https://cdn.mxpnl.com/libs/mixpanel-2-latest.min.js
+  'script-src': ["'self'", "https://polly-bot-server.herokuapp.com/app" ],
+
+  // Allow fonts to be loaded from http://fonts.gstatic.com
+  'font-src': ["'self'", "https://polly-bot-server.herokuapp.com/app"],
+
+  // Allow data (xhr/websocket) from api.mixpanel.com and custom-api.local
+  'connect-src': ["'self'", "https://polly-bot-server.herokuapp.com/app" ],
+
+  // Allow images from the origin itself (i.e. current domain)
+  'img-src': "'self'", 
+
+  // Allow CSS loaded from https://fonts.googleapis.com
+  'style-src': ["'self'", "https://fonts.googleapis.com", "https://polly-bot-server.herokuapp.com/app"],
+
+  // Omit `media-src` from policy
+  // Browser will fallback to default-src for media resources (which is 'none', see above)
+  'media-src': null
+}
+
   return ENV;
 };
