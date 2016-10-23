087c26a6c9e06306e138551da3436bdcba54bbb0
Elton Murillo <emurillo510@gmail.com>
2016-10-23 04:56:31 -0700

    Adds chatbot functionality.

--- app/chatbot/controller.js
+++ app/chatbot/controller.js
@@ -1,25 +1,46 @@
 import Ember from 'ember';
 
-let watsonUrl = "api";
+let GREETING_MESSAGE = "Hi, I'm Polly! I'm here to help you research your fair salary.";
+let ASKING_NAME_MESSAGE = "First, I need to learn a little about you. What's your name?";
+let IDENTIFY_EDU_MESSAGE = "Nice to meet you, Jane. Let's get started. What's your highest earned degree of level of school?";
+let IDENTIFY_INDUSTRY = "What industry are you currently in?";
+let IDENTIFY_SALARY = "";
+
+/*
+
+{
+  "url": "https://gateway.watsonplatform.net/conversation/api",
+  "password": "sMBUnNFWNXop",
+  "username": "fa17b65f-d5c7-4465-914e-464c73a28ed1"
+}
+
+*/
+
 export default Ember.Controller.extend({
+    isGreeting: true,
+    isEducation: false,
+    init(){
+        console.log('page loaded....');
+    },
+    state: "GREETING",
     actions: {
         submit(){
 
-            let chatbox = $('#polly-messages');
-            let userMessage = $('#input-chatbot').val();
+            let state = this.get('state');
 
-            console.log('userMessage', userMessage);
-            chatbox.append(userMessage + '<br/>');
+            if(state === "GREETING"){
+                console.log('state', state);
+                this.set('isGreeting', false);
+                this.set('isEducation', true);
+                console.log(this.get('isGreeting'));
+                console.log(this.get('isEducation'));
+
+                var route = this.get('route');
+                route.refresh();
+
+            }
 
 
-            alert('send this to watson' + userMessage);
-           /* 
-            $(watsonUrl).post(userMessage, function(response){
-                let data = response;
-
-
-            });
-            */
         }
     }
     
